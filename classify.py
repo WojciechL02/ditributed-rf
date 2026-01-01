c59c058e34b9963259a48d57f9bfed348c9b5ef9
WojciechL02 <wojciech.lapacz02@gmail.com>
2026-01-01 18:42:39 +0100

    distributed training scripts

--- classify.py
+++ classify.py
@@ -1,12 +1,52 @@
+import os
+import pickle
 import argparse
+import pandas as pd
+import torch
+import torch.distributed as dist
+from rf import RandomForest
+from utils import vectorize
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("model-input", type=str, required=False, default="model")
-    parser.add_argument("query-input", type=str, required=False, default="queries")
-    parser.add_argument("predictions-output", type=str, required=False, default="results")
+    parser.add_argument("model_input", type=str)
+    parser.add_argument("n_trees", type=int)
+    parser.add_argument("query_input", type=str)
+    parser.add_argument("predictions_output", type=str)
     args = parser.parse_args()
+
+    # distributed setup
+    # WORLD_SIZE = int(os.environ['OMPI_COMM_WORLD_SIZE'])
+    # RANK = int(os.environ['OMPI_COMM_WORLD_RANK'])
+    WORLD_SIZE = int(os.environ['WORLD_SIZE'])
+    RANK = int(os.environ['RANK'])
+    dist.init_process_group("gloo", rank=RANK, world_size=WORLD_SIZE)
+    if RANK == 0:
+        print(f"Starting distributed classification with {WORLD_SIZE} processes.")
+
+    # load data
+    df = pd.read_csv(f"{args.query_input}.csv")
+    with open("data/vocab.pkl", "rb") as f:
+        vocab = pickle.load(f)
+    data_X = vectorize(df["text"], vocab)
+    data_y = torch.tensor(df["label"].values)
+
+    # load model
+    rf = RandomForest(RANK, WORLD_SIZE, args.n_trees, max_depth=10, seed=42)
+    rf.load_from_file(args.model_input)
+    preds = rf.predict(data_X)
+
+    if RANK == 0:
+        correct = (data_y == preds).float()
+        acc = correct.mean().item()
+        print(f"NPROC={WORLD_SIZE} | Acc: {acc:.3f}")
+        with open(f"{args.predictions_output}_{WORLD_SIZE}.txt", "w") as f:
+            for pred in preds:
+                f.write(f"{pred}\n")
+
+    dist.destroy_process_group()
+
 
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -1,13 +1,52 @@
+import os
+import time
+import pickle
 import argparse
+import pandas as pd
+import torch
+import torch.distributed as dist
+from rf import RandomForest
+from utils import vectorize
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("dataset-path", type=str, required=True)
-    parser.add_argument("model-output", type=str, required=False, default="model")
-    parser.add_argument("n-trees", type=int, required=True)
-    parser.add_argument("seed", type=int, required=False, default=42)
+    parser.add_argument("dataset_path", type=str)
+    parser.add_argument("model_output", type=str)
+    parser.add_argument("n_trees", type=int)
+    parser.add_argument("--seed", type=int, default=42)
     args = parser.parse_args()
+
+    # distributed setup
+    # WORLD_SIZE = int(os.environ['OMPI_COMM_WORLD_SIZE'])
+    # RANK = int(os.environ['OMPI_COMM_WORLD_RANK'])
+    WORLD_SIZE = int(os.environ['WORLD_SIZE'])
+    RANK = int(os.environ['RANK'])
+    dist.init_process_group("gloo", rank=RANK, world_size=WORLD_SIZE)
+    if RANK == 0:
+        print(f"Starting distributed training with {WORLD_SIZE} processes.")
+
+    # load data
+    df = pd.read_csv(f"{args.dataset_path}_{RANK}.csv")
+    with open("data/vocab.pkl", "rb") as f:
+        vocab = pickle.load(f)
+    data_X = vectorize(df["text"], vocab)
+    data_y = torch.tensor(df["label"].values)
+
+    # create and train model
+    rf = RandomForest(RANK, WORLD_SIZE, args.n_trees, max_depth=10, seed=args.seed)
+    dist.barrier()
+    start = time.time()
+    rf.fit(data_X, data_y)
+    dist.barrier()
+    end = time.time()
+    rf.save_to_file(args.model_output)
+    if RANK == 0:
+        parallel_time = end - start
+        print(f"NPROC={WORLD_SIZE} | NTrees={args.n_trees} | Time: {parallel_time:.4f}s")
+
+    dist.destroy_process_group()
+
 
 if __name__ == "__main__":
     main()
